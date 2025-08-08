1f456012fdfc5b29f7939d4926fc8efbbe074bec
Sarinha <eliaxarcanjo@gmail.com>
2025-08-08 00:43:26 -0300

    changing url again

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://frontend-nippon.vercel.app", // ou use uma variável de ambiente depois
+    origin: "http://localhost:5173", // ou use uma variável de ambiente depois
     credentials: true,
   })
 );
